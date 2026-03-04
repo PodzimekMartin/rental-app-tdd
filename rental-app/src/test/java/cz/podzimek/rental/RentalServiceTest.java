4628904a4655e772aee67b316ab4bb557c18179a
PodzimekMartin <126859478+PodzimekMartin@users.noreply.github.com>
2026-03-04 15:35:32 +0100

    test: limit customer to max 3 active rentals

--- rental-app/src/test/java/cz/podzimek/rental/RentalServiceTest.java
+++ rental-app/src/test/java/cz/podzimek/rental/RentalServiceTest.java
@@ -1,20 +1,30 @@
 package cz.podzimek.rental;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RentalServiceTest {
 
     @Test
     void itemCannotBeRentedTwice() {
-
         RentalService service = new RentalService();
 
         service.rentItem("item1");
 
-        assertThrows(IllegalStateException.class, () -> {
-            service.rentItem("item1");
-        });
+        assertThrows(IllegalStateException.class, () -> service.rentItem("item1"));
+    }
 
+    @Test
+    void customerCannotHaveMoreThanThreeActiveRentals() {
+        RentalService service = new RentalService();
+
+        service.rentItemForCustomer("customer1", "item1");
+        service.rentItemForCustomer("customer1", "item2");
+        service.rentItemForCustomer("customer1", "item3");
+
+        assertThrows(IllegalStateException.class, () ->
+                service.rentItemForCustomer("customer1", "item4")
+        );
     }
 }
